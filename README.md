401a60cab3b78ef6b77a8b02e306120043999c15
Mike Bierlee <m.bierlee@lostmoment.com>
2022-10-31 21:46:04 +0300

    Add project skeleton

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mirage Config injector for Poodinis
+
+Version 0.0.0  
+Copyright 2022 Mike Bierlee  
+Licensed under the terms of the MIT license - See [LICENSE.txt](LICENSE.txt)
+
+More will follow...
---
+++ source/poodinis/valueinjector/mirage.d
@@ -0,0 +1,8 @@
+/**
+ * Poodinis Dependency Injection Framework
+ * Copyright 2022 Mike Bierlee
+ * This software is licensed under the terms of the MIT license.
+ * The full terms of the license can be found in the LICENSE file.
+ */
+
+module poodinis.valueinjector.mirage;
